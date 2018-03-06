262cf7d0ac062b5b6cf3675f215d031b536b4a76
Brian Atwood <brianatwood@Brians-MacBook-Pro-2.local>
2018-03-06 15:47:44 -0600

    solutions

--- Vector.py
+++ Vector.py
@@ -19,3 +19,33 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def __add__(self, v):
+        sum = []
+        for i in range(len(self.coordinates)):
+            sum.append(self.coordinates[i] + v.coordinates[i])
+        return sum
+
+    def __sub__(self, v):
+        sum = []
+        for i in range(len(self.coordinates)):
+            sum.append(self.coordinates[i] - v.coordinates[i])
+        return sum
+
+    def __mul__(self, multiplier):
+        return [dim * multiplier for dim in self.coordinates]
+
+
+a = Vector([8.218, -9.341])
+b = Vector([-1.129, 2.111])
+print(a + b)
+
+
+# a = Vector([7.119, 8.215])
+# b = Vector([-8.223, 0.878])
+# print(a - b)
+
+
+# a = Vector([1.671, -1.012, -0.318])
+
+# print(a * 7.41)
